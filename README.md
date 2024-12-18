4e5258755c2113b6a594ba475e57c7fc7357dba6
Udit Yadav <49510164+iamudityadav@users.noreply.github.com>
2024-12-18 19:14:51 +0000

    fixed variable naming and readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# webmobb
+# Fetch all swap transactions occurring on the USDC/WETH pool
+
+This repository allows fetching all the swap transactions occurring in the USDC/WETH pool in Uniswap V3.
+
+## Description
+This repository listens to swap events in the USDC/WETH Uniswap V3 pool on the Ethereum mainnet using WebSocket. It creates a "Swap" object and prints that object. It determines if a transaction is a buy or sell transaction with respect to ETH.
+
+
+## Installation
+
+Follow these steps to get this project up and running locally:
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/iamudityadav/fetch-swap-tx
+
+2. Navigate into the project directory:
+   ```bash
+   cd repository
+
+3. Install dependencies::
+   ```bash
+    npm install
+
+## Running the Project
+```bash
+    node src/index.js
--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 const {ethers} = require("ethers");
 
+const provider = new ethers.WebSocketProvider("wss://eth.drpc.org");
+
 const poolABI = [
     "event Swap(address indexed sender,address indexed recipient,int256 amount0,int256 amount1,uint160 sqrtPriceX96,uint128 liquidity,int24 tick)"
 ];
-
 const poolAddress = "0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640"; 
-const provider = new ethers.WebSocketProvider("wss://eth-mainnet.g.alchemy.com/v2/d02qnkslydvXc4c7QA4wruO66UFF7R-F");
-const wethUsdcContract = new ethers.Contract(poolAddress, poolABI, provider);
+const usdcWethContract = new ethers.Contract(poolAddress, poolABI, provider);
 
-const usdcAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";       //token0
-const wethAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";       //token1
+
 const erc20ABI = [
     "function decimals() view returns (uint8)"
 ];
+const usdcAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";       //token0
+const wethAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";       //token1
 const usdcContract = new ethers.Contract(usdcAddress, erc20ABI, provider);
 const wethContract = new ethers.Contract(wethAddress, erc20ABI, provider);
 
@@ -27,7 +28,7 @@ class Swap{
     }
 }
 
-wethUsdcContract.on(poolABI[0], async (sender,recipient,amount0,amount1,sqrtPriceX96,liquidity,tick) => {
+usdcWethContract.on(poolABI[0], async (sender,recipient,amount0,amount1,sqrtPriceX96,liquidity,tick) => {
     const price = (Number(sqrtPriceX96) ** 2)/ 2 ** 192;
 
     const usdcDecimals = await usdcContract.decimals();     //token0
@@ -48,7 +49,7 @@ wethUsdcContract.on(poolABI[0], async (sender,recipient,amount0,amount1,sqrtPric
     }
 
     const newSwap = new Swap(sender.toString(), recipient.toString(), baseTokenAmount, quoteTokenAmount, txType, price);
-    console.log("New Swap: ", newSwap);
+    console.log(newSwap);
 
     // console.log(":::::::::::::::::::::Swap detected:::::::::::::::::::::");
     // console.log("Sender: ", sender);
